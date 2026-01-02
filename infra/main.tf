0a6222db5d6e637341cb95e070b5dc1793bfa7e0
Mahdi Ben <hocine.mohamed213@gmail.com>
2026-01-01 23:06:58 -0500

    Add adds CF function to use index.html with CF URL

--- infra/main.tf
+++ infra/main.tf
@@ -19,6 +19,32 @@ resource "aws_cloudfront_origin_access_control" "oac" {
   signing_protocol                  = "sigv4"
 }
 
+resource "aws_cloudfront_function" "url_rewrite" {
+  name    = "url-rewrite-index"
+  runtime = "cloudfront-js-1.0"
+  comment = "Append index.html to directory requests"
+  publish = true
+  
+  code = <<EOF
+function handler(event) {
+    var request = event.request;
+    var uri = request.uri;
+    
+    // Check whether the URI is missing a file name
+    if (uri.endsWith('/')) {
+        request.uri += 'index.html';
+    } 
+    // Check whether the URI is missing a file extension
+    else if (!uri.includes('.')) {
+        request.uri += '/index.html';
+    }
+    
+    return request;
+}
+EOF
+}
+
+
 resource "aws_cloudfront_distribution" "blog" {
   enabled             = true
   is_ipv6_enabled     = true  # Add this - your current CF has it enabled
